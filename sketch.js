c14e48f7fd58062b6f88fb677291f9644529f671
okirpane <31450994+okirpane@users.noreply.github.com>
2018-06-05 14:08:35 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -9,22 +9,23 @@ function setup(){
 	noStroke();
 	colorMode(HSB, 100);
 	capture = createCapture(VIDEO);
-	capture.size(640, 800);
+	capture.size(width, height);
 	capture.hide();
 	fill(0);
 }
 function draw(){
 	background(255);
+	
+	//image(capture, 0, 0, width, height);
 	capture.loadPixels();
-	//image(capture, width/2-capture.width/2, height/2-capture.height/2, 640, 480);
 	var size = round(constrain(8, 6, 32));
-	for (var x = 0; x < height; x+= size){
-		for (var y = 0; y < width; y += size){
+	for (var x = 0; x < width; x+= size){
+		for (var y = 0; y < height; y += size){
 			var i = y * width + x;
 			var darkness = (255 - capture.pixels[i*4])/255;
 			var radius = size * darkness;
 			fill(h, 200, 100);
-			ellipse(x + (width/2-capture.width/2), y + 150 + (height/2-capture.height/2), radius, radius);
+			ellipse(x , y, radius, radius);
 		}
 	}
 	fill(h, 150, 100);
